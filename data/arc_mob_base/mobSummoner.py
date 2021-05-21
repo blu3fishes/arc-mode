1f77db15741d7da8040937ebfbc460a23b8c350e
blu3fishez <kcw2205@naver.com>
2021-05-21 17:21:50 +0900

    Added Code-automation programs

--- data/arc_mob_base/mobSummoner.py
+++ data/arc_mob_base/mobSummoner.py
@@ -9,6 +9,8 @@
 
 ## Import
 import os
+from tkinter import *
+import tkinter
 ## Definition
 def createFolder(directory):
     try:
@@ -16,38 +18,45 @@ def createFolder(directory):
             os.makedirs(directory)
     except OSError:
         print ('Error: Creating directory. ' +  directory)
-NameBuf = input("Function name? : ")
-NameSpaceBuf = input("Input NameSpace e.g. arc_mode etc..  : ")
-print("")
-SpaceBuf = input("Input Folder Name e.g. level_one (Press Enter If none.): ")
-print("\nInput Summon Command (e.g. summon ~ ~ ~ creeper{...}")
-CmdBuf = input("Should be like e.g. : ")
 
-createFolder('./functions')
-createFolder("./functions/mobs/")
-createFolder('./functions/mobs/SpaceBuf')
+def makeMobFeature():
+    '''
+    add tags if the mob has advanced attribute.
+    '''
+def makeFiles(): 
+    NameBuf = input("Function name? : ")
+    NameSpaceBuf = input("Input NameSpace e.g. arc_mode etc..  : ")
+    print("")
+    SpaceBuf = input("Input Folder Name e.g. level_one (Press Enter If none.): ")
+    print("\nInput Summon Command (e.g. summon ~ ~ ~ creeper{...}")
+    CmdBuf = input("Should be like e.g. : ")
 
-MobNameEnd = int(-1)
-for i in range(0, len(CmdBuf)):
-    if CmdBuf[i] == '{':
-        MobNameEnd = i + 1
-        break
-if MobNameEnd == -1:
-    MobNameEnd = len(CmdBuf)
+    createFolder('./functions')
+    createFolder("./functions/mobs/")
+    createFolder('./functions/mobs/SpaceBuf')
 
-MobName = CmdBuf[13:MobNameEnd]
+    MobNameEnd = int(-1)
+    for i in range(0, len(CmdBuf)):
+        if CmdBuf[i] == '{':
+            MobNameEnd = i + 1
+            break
+    if MobNameEnd == -1:
+        MobNameEnd = len(CmdBuf)
 
-if SpaceBuf != '':
-    SpaceBuf = SpaceBuf + '/'
+    MobName = CmdBuf[13:MobNameEnd]
 
-set_mob = open('./functions/set_mob.mcfunction', mode="at", encoding="utf-8")
-set_mob.write("execute as @e[type=%s,tag=!check,sort=random] at @s run function %s:mobs/%s%s\n" %(MobName, NameSpaceBuf, SpaceBuf, NameBuf))
-set_mob.close()
+    if SpaceBuf != '':
+        SpaceBuf = SpaceBuf + '/'
 
-mob_file = open('./functions/mobs/%s%s.mcfunction' %(SpaceBuf, NameBuf), mode="wt", encoding="utf-8")
-mob_file.write("""tp @e[type=chicken,tag=!check] ~ ~-400 ~
-summon chicken ~ ~ ~ {Tags:["check"],Attributes:[{Name:generic.max_health,Base:16},{Name:generic.movement_speed,Base:0.3}]}""")
-mob_file.close()
+    set_mob = open('./functions/set_mob.mcfunction', mode="at", encoding="utf-8")
+    set_mob.write("execute as @e[type=%s,tag=!check,sort=random] at @s run function %s:mobs/%s%s\n" %(MobName, NameSpaceBuf, SpaceBuf, NameBuf))
+    set_mob.close()
 
-print("___PROCESS END__")
-input("Press Any Key To Close...")
+    mob_file = open('./functions/mobs/%s%s.mcfunction' %(SpaceBuf, NameBuf), mode="wt", encoding="utf-8")
+    mob_file.write("""tp @e[type=chicken,tag=!check] ~ ~-400 ~\n""")
+    # find {, and append the Tag which name is 'check'.
+    # add Tag if the mob is advanced mob. this will be come with radio checkbox.
+    #summon chicken ~ ~ ~ {Tags:["check"],Attributes:[{Name:generic.max_health,Base:16},{Name:generic.movement_speed,Base:0.3}]}""")
+    mob_file.close()
+
+mainWindow = Tk()
